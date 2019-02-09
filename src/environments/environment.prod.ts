2123482f625d23a3700fc6d9259af6802a3a20ad
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-09 11:31:05 -0500

    added endpointURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endpointURL: 'http://localhost/bosch2/wp-json/wp/v2/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  endpointURL: 'http://localhost/bosch2/wp-json/wp/v2/'
 };
 
 /*
