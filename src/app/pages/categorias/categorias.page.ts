b8498c02e3237e8971f96007675aa3e58c56d189
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-09 11:30:38 -0500

    added categories inside ion-cards

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-categorias',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasPage implements OnInit {
 
-  constructor() { }
+  categorias: any[] = [];
+  productos: any[] = [];
+
+  constructor(
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+    this.getCategoriasProductos();
+    this.getProductos();
+  }
+
+  getCategoriasProductos() {
+    this.dataService.getCategoriasProductos().subscribe((data: any[]) => {
+      this.categorias = data;
+      console.log('ngOnInit() > items: %o', this.categorias);
+    });
+  }
+
+  getProductos() {
+    this.dataService.getAllProducts().subscribe((data: any[]) => {
+      this.productos = data;
+      console.log('ngOnInit() > items: %o', this.productos);
+    });
   }
 
 }
