8b4cb480dfa1c3088cc46ef3afcc0b5e7f2cb942
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-10 16:40:34 -0500

    added helpers service

---
+++ src/app/shared/services/helpers.service.ts
@@ -0,0 +1,149 @@
+import { Injectable } from '@angular/core';
+import { LoadingController, ToastController, ModalController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelpersService {
+
+  modal: HTMLIonModalElement;
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private modalCtrl: ModalController
+  ) { }
+
+  async presentLoading(message: string = '') {
+    const loading = await this.loadingCtrl.create({
+      message: message
+    });
+    return loading;
+  }
+
+  async presentToast(message: string = '') {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'middle',
+      cssClass: 'toastHelper'
+    });
+    toast.present();
+  }
+
+  /**
+   * WORKING WITH IMAGES (UPLOADING...)
+   */
+  processing: boolean;
+  uploadImage: string;
+
+  /**
+   * Presents image loader action window & selects the image
+   * @param fileLoader 
+   */
+  presentActionSheet(fileLoader) {
+    fileLoader.click();
+    var that = this;
+    fileLoader.onchange = function () {
+      var file = fileLoader.files[0];
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function () {
+        that.processing = true;
+        that.getOrientation(fileLoader.files[0], function (orientation) {
+          if (orientation > 1) {
+            that.resetOrientation(reader.result, orientation, function (resetBase64Image) {
+              that.uploadImage = resetBase64Image;
+            });
+          } else {
+            that.uploadImage = reader.result;
+          }
+        });
+      }, false);
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+  
+  /**
+   * Gets the image orientation
+   * @param file 
+   * @param callback 
+   */
+  getOrientation(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e:any) {
+
+      var view = new DataView(e.target.result);
+      if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+      var length = view.byteLength, offset = 2;
+      while (offset < length) {
+        var marker = view.getUint16(offset, false);
+        offset += 2;
+        if (marker == 0xFFE1) {
+          if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+          for (var i = 0; i < tags; i++)
+            if (view.getUint16(offset + (i * 12), little) == 0x0112)
+              return callback(view.getUint16(offset + (i * 12) + 8, little));
+        }
+        else if ((marker & 0xFF00) != 0xFF00) break;
+        else offset += view.getUint16(offset, false);
+      }
+      return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  /**
+   * Resets image orientation
+   */
+  resetOrientation(srcBase64, srcOrientation, callback) {
+    var img = new Image();
+
+    img.onload = function () {
+      var width = img.width,
+        height = img.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d");
+
+      // set proper canvas dimensions before transform & export
+      if (4 < srcOrientation && srcOrientation < 9) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      }
+
+      // transform context before drawing image
+      switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+        case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+        case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+        case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+        default: break;
+      }
+
+      // draw image
+      ctx.drawImage(img, 0, 0);
+
+      // export base64
+      callback(canvas.toDataURL());
+    };
+
+    img.src = srcBase64;
+  }
+
+  /**
+   * END WORKING WITH IMAGES (UPLOADING...)
+   */
+}
