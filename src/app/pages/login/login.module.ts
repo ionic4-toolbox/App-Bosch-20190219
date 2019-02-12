aedf3afa096aeb3cda37d47b98684a2a0898cd3d
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-12 08:07:26 -0500

    updated login module

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 const routes: Routes = [
   {
@@ -22,6 +23,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  providers: [StorageService]
 })
 export class LoginPageModule {}
