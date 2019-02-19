2c88762ebeb1fef9cef8d6ef5bf01737c3f2c66e
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-18 19:05:47 -0500

    fetched user installations & designed page

--- src/app/pages/user-installations/user-installations.page.ts
+++ src/app/pages/user-installations/user-installations.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserInstallationsService } from './user-installations.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-installations',
   templateUrl: './user-installations.page.html',
   styleUrls: ['./user-installations.page.scss'],
 })
-export class UserInstallationsPage implements OnInit {
+export class UserInstallationsPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscriptions: Subscription = new Subscription();
+  installations: any[];
+
+  constructor(
+    private userInstallationService: UserInstallationsService
+  ) { }
 
   ngOnInit() {
+    this.getUserInstallations();
+  }
+
+  getUserInstallations() {
+    this.subscriptions.add(
+      this.userInstallationService.getUserInstallations(44).subscribe((res: any) => {
+        this.installations = res;
+        console.log(res);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
 }
--- src/app/pages/user-installations/user-installations.service.ts
+++ src/app/pages/user-installations/user-installations.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+const ENDPOINT_URL = environment.endpointURL;
 @Injectable({
   providedIn: 'root'
 })
 export class UserInstallationsService {
 
-  constructor() { }
+  installations: any[];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUserInstallations(idAuthor: number) {
+    if (this.installations) {
+      return of(this.installations);
+    }
+    return this.http.get(ENDPOINT_URL + 'instalacion?author=' + idAuthor).pipe(
+      map((i: any) => this.installations = i)
+    );
+  }
 }
