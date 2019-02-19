a5f27a344bf106708c5cddf662d48b9690c87f0c
Alain Iglesias <alain.iherrera@gmail.com>
2019-02-19 13:14:12 -0500

    added press twice hardware back button to close app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { HelpersService } from './shared/services/helpers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  lastTimeBackPress = 0;
+  timePeriodToExit = 2000;
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    private helpersService: HelpersService
   ) {
     this.initializeApp();
+
+    this.hardwareBackButton();
   }
 
   initializeApp() {
@@ -22,5 +33,22 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  hardwareBackButton(){
+    this.platform.backButton.subscribe(async () =>{
+      this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+        } else if (this.router.url === '/' || this.router.url === '/dashboard') {
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            navigator['app'].exitApp();
+          } else {
+            this.helpersService.presentToast('Presione nuevamente para cerrar la aplicación');                    
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        }
+      });
+    });    
   }
 }
